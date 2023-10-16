5a6d85458de3a330d440c357423e4d64a87423b9
Arshdeep-ceo <punjabistudios18@gmail.com>
2023-10-16 12:52:37 +0530

    Added about, contact

--- src/features/about/About.jsx
+++ src/features/about/About.jsx
@@ -1,5 +1,221 @@
-function About() {
-  return <div>About Page</div>;
-}
+import { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+import { Avatar } from "@nextui-org/react";
 
-export default About;
+const links = [
+  { name: "Offline courses", href: "#" },
+  { name: "Internship program", href: "#" },
+  { name: "Our values", href: "#" },
+  { name: "Meet our leadership", href: "#" },
+];
+const stats = [
+  { name: "Offices worldwide", value: "1" },
+  { name: "Full-time colleagues", value: "30+" },
+  { name: "Hours per week", value: "40" },
+  { name: "Paid time off", value: "Unlimited" },
+];
+
+export default function About() {
+  const people = [
+    {
+      imageUrl: "../../public/images/office.bg.jpeg",
+    },
+    {
+      imageUrl: "../../public/images/team.bg.jpeg",
+    },
+    {
+      imageUrl: "../../public/images/meet.bg.jpeg",
+    },
+    {
+      imageUrl: "../../public/images/meet2.bg.jpeg",
+    },
+
+    // More people...
+  ];
+  const team = [
+    {
+      name: "Arshdeep Singh",
+      role: "Web Developer",
+      imageUrl: "../../public/images/team/t2.webp",
+    },
+    {
+      name: "Sunil Jaswal",
+      role: "Developer",
+      imageUrl: "../../public/images/team/t1.webp",
+    },
+
+    {
+      name: "Ramanpreet kaur",
+      role: "Web Developer",
+      imageUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+
+    // More people...
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? people.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === people.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (peopleIndex) => {
+    setCurrentIndex(peopleIndex);
+  };
+  return (
+    <div className="pr-0 flex overflow-hidden bg-white-800 pt-4 pl-3  ">
+      {/* <img
+        src="https://images.unsplash.com/photo-1610465299993-e6675c9f9efa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+        alt=""
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      /> */}
+      {/* <div // ki aa eh div oh taan menu vi dikhda eh ki answer aa  oh yr code kito copy krea kito kuch elaemdednt krebaol na kerha eh element bol kerha ki krda eh code eh ta ma dhyan ni dita
+        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" // aa ki kr ryi dance
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          // style={{
+          //   clipPath:
+          //     "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          // }}
+        />
+      </div> */}
+      <div
+        className="fixed -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
+      >
+        <div
+          className="absolute aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+
+      {/* <div className="mx-auto max-w-7xl px-6 lg:px-8"> */}
+      <div className="">
+        <div className=" mx-auto max-w-2xl lg:mx-0">
+          <h2 className=" text-[2.8rem] font-bold bg-clip-text bg-gradient-to-r from-pink-600 via-amber-700 to bg-amber-600 tracking-tight text-transparent sm:text-6xl animate-[bounce_5s_ease-in_infinite] duration-10000 ">
+            Work with us
+          </h2>
+          <p className=" text-md leading-6 text-gray-800 animate-drip-expand">
+            Learn with Expert Learning with Experts is a global classroom
+            community that brings people together to learn from expert tutors
+            and each other.
+          </p>
+          <p className=" mt-5 text-3xl tracking-tight text-black sm:text-4xl animate-pulse w-15">
+            We Provide
+          </p>
+        </div>
+        {/* <div className="bg-slate-100"></div> */}
+        <div className="mt-5">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-black sm:grid-cols-2 md:flex lg:gap-x-10">
+            {links.map((link) => (
+              <a key={link.name} href={link.href}>
+                {link.name} <span aria-hidden="true">&rarr;</span>
+              </a>
+            ))}
+          </div>
+          <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4 animate-appearance-in">
+            {stats.map((stat) => (
+              <div key={stat.name} className="flex flex-col-reverse">
+                <dt className="text-base leading-7 text-black">{stat.name}</dt>
+                <dd className="text-2xl font-bold leading-9 tracking-tight text-black">
+                  {stat.value}
+                </dd>
+              </div>
+            ))}
+          </dl>
+
+          <div className=" pl-5 pr-4 pt-24 sm:pt-14 mt-8  bg-gradient-to-r from-pink-600 via-amber-400 to bg-amber-600 ">
+            <div className="max-w-2xl">
+              <h2 className="ml-0 text-3xl  tracking-tight sm:text-6xl animate-[bounce_5s_ease-in_infinite] duration-10000">
+                Our Awesome Team!
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-800">
+                Meet the leaders behind the worlds leading software development
+                platform.
+              </p>
+            </div>
+            <ul // Team Cards
+              role="list"
+              className="mt-6 grid grid-cols-1 gap-x-2 gap-y-12 sm:grid-cols-3 sm:gap-y-10 sm:gap-x-4   xl:col-span-2 place-content-stretch place-items-stretch mb-8" //
+            >
+              {team.map((person) => (
+                <li key={person.name} className="text-center ">
+                  {/* <img
+                    className=" rounded-md w-[100%] p-3"
+                    src={person.imageUrl}
+                    alt=""
+                  /> */}
+                  <Avatar
+                    isBordered
+                    radius="sm"
+                    color="primary"
+                    src={person.imageUrl}
+                    className="h-30 w-unit-5xl ring-transparent drop-shadow-xl"
+                  />
+
+                  <h3 className=" text-left font-semibold leading-7 tracking-tight text-gray-900">
+                    {person.name}
+                  </h3>
+                  <p className="text-left text-sm font-semibold leading-6 text-amber-1000">
+                    {person.role}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className=" bg-gradient-to-r from-amber-200 via-amber-100 to bg-amber-600 py-24 sm:py-22">
+            <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+              <div className="max-w-2xl">
+                <h2 className="text-4xl font-bold  bg-clip-text bg-gradient-to-r from-pink-600 via-amber-700 to bg-amber-600 tracking-tight text-transparent sm:text-6xl animate-[bounce_5s_ease-in_infinite] duration-10000 ">
+                  Some interesting stories!{" "}
+                </h2>
+                <p className="mt-6 text-lg font-serif leading-8 text-gray-600">
+                  Learn more about our dedicated, focused and experienced
+                  leadership team ...
+                </p>
+              </div>
+            </div>
+            <div className="mt-6 max-w-[1400px] h-[700px] w-full m-auto py-16 px-4 relative group ">
+              <div
+                style={{
+                  backgroundImage: `url(${people[currentIndex].imageUrl})`,
+                }}
+                className="w-full h-full rounded-2xl bg-cover bg-scroll duration-500 top-0 left-0 absolute"
+              ></div>
+              <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactLeft onClick={prevSlide} size={30} />
+              </div>
+              <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactRight onClick={nextSlide} size={30} />
+              </div>
+              <div className="flex top-4  justify-center py-2">
+                {people.map((peoples, peopleIndex) => {
+                  <div
+                    key={peopleIndex}
+                    onClick={() => goToSlide(peopleIndex)}
+                    className="text-2xl cursor-pointer"
+                  >
+                    <RxDotFilled />
+                  </div>;
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -1,5 +1,295 @@
-function Contact() {
-  return <div>Contact</div>;
+import { useState } from "react";
+// import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Switch } from "@headlessui/react";
+// import { Header } from "styled-icons/fa-solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
 }
 
-export default Contact;
+export default function Contact() {
+  const [agreed, setAgreed] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    course: "",
+    email: "",
+    phoneNumber: "",
+    skills: "",
+  });
+
+  let name, value;
+  const getUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+    setUser({ ...user, [name]: value });
+  };
+
+  const postData = async (e) => {
+    e.preventDefault();
+    const { name, course, email, phoneNumber, skills } = user;
+    if (name && course && email && phoneNumber && skills) {
+      const res = await fetch(
+        "https://eaglecodersforms-default-rtdb.firebaseio.com/contactusform.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            course,
+            email,
+            phoneNumber,
+            skills,
+          }),
+        },
+      );
+      if (res) {
+        setUser({
+          name: "",
+          course: "",
+          email: "",
+          phoneNumber: "",
+          skills: "",
+        });
+        alert("Data Stored Successfully!😍");
+      }
+    } else {
+      alert("Please fill all the data!😊");
+    }
+  };
+
+  return (
+    <div className=" pl-6 w-full h-full rounded-2xl bg-cover bg-scroll duration-500 top-0 left-0 relative ">
+      <div className=" lg:mx-0 bg-transparent">
+        <img
+          className="mx-auto h-unit-4xl animate-drip-expand"
+          src="../../public/images/logo.png"
+          alt=""
+        />
+        <figure className="mt-3  ">
+          <blockquote className=" animate-animate_floating text-center text-xl font-bold leading-8 text-gray-900 sm:text-5xl sm:leading-9">
+            <p className="">Contact our friendly team</p>
+          </blockquote>
+          <blockquote className=" ani mt-3 text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+            <p className=" animate-[appearance-in_3s_ease-in] duration-1000">
+              Let us know how we can help..
+            </p>
+            We are open for any suggestion or just to have a chat
+          </blockquote>
+        </figure>
+      </div>
+      <div className="text-center mt-12 grid grid-cols-3 grid-flow-col gap-4 h-40 ml-4 mr-50 ">
+        <div className="shadow-large ">
+          <div className=" font-bold">Chat to support</div>
+          <p className="font-semibold">We are here to help</p>
+          <img
+            className="mx-auto h-unit-2xl mt-3"
+            src="../../public/images/logo.png"
+            alt=""
+          />
+          <p className="underline mt-3">theeaglecoders@gmail.com</p>
+        </div>
+
+        <div className="shadow-large">
+          <div className=" font-bold">Vist us</div>
+
+          <p className="mt-3 underline">Visit our office HQ.</p>
+          <p className="font-semibold mt-4">
+            d, 185D, Phase 8B, Industrial Area, Sector 74, Sahibzada Ajit Singh
+            Nagar, Punjab 160055
+          </p>
+        </div>
+        <div className="shadow-large">
+          <div className=" font-bold">Call us</div>
+          <p className=" font-semibold mt-1">Speak to our friendly team</p>
+          <img
+            className="mx-auto h-unit-2xl mt-3"
+            src="../../public/images/logo.png"
+            alt=""
+          />
+          <p className=" font-semibold underline mt-2">+91 88723 12729</p>
+        </div>
+      </div>
+
+      <div className=" rounded-large mt-8 mb-6 bg-gradient-to-r from-pink-600 via-amber-700 to bg-amber-600">
+        <div
+          className=" absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className=" rounded-xl shadow-2xl mt-6 pt-6 mx-auto max-w-2xl text-center">
+          <h2 className=" animate-bounce font-bold tracking-tight  bg-gradient-to-r from-pink-600 via-amber-500 to bg-amber-600 sm:text-5xl">
+            Contact Us
+          </h2>
+          <p className="mt-12 text-lg leading-8 bg-clip-text bg-gradient-to-r from-pink-500 via-amber-500 to bg-amber-600">
+            Contact the Eagle Coders® here by Email Skype or Contact form - We
+            look forward to starting a conversation with you!
+          </p>
+        </div>
+
+        <form
+          action="#"
+          method="POST"
+          className=" animate-appearance-in mx-auto mt-16 max-w-xl sm:mt-20"
+        >
+          <div className=" grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="company"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Enter your name"
+                  value={user.name}
+                  onChange={getUserData}
+                  autoComplete="name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="company"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Course
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="course"
+                  id="course"
+                  placeholder="Enter your course name"
+                  value={user.course}
+                  onChange={getUserData}
+                  autoComplete="course"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email-address"
+                  value={user.email}
+                  onChange={getUserData}
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="company"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Mobile number
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  id="phoneNumber"
+                  placeholder="Enter your phone number"
+                  value={user.phoneNumber}
+                  onChange={getUserData}
+                  autoComplete="course"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="company"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Skills you want to learn
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="skills"
+                  id="skills"
+                  placeholder="Enter skills you want to learn"
+                  value={user.skills}
+                  onChange={getUserData}
+                  autoComplete="skills"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+              <div className="flex h-6 items-center">
+                <Switch
+                  checked={agreed}
+                  onChange={setAgreed}
+                  className={classNames(
+                    agreed ? "bg-indigo-600" : "bg-gray-200",
+                    "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
+                  )}
+                >
+                  <span className="sr-only">Agree to policies</span>
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      agreed ? "translate-x-3.5" : "translate-x-0",
+                      "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out",
+                    )}
+                  />
+                </Switch>
+              </div>
+              <Switch.Label className="text-sm leading-6 text-gray-600">
+                By selecting this, you agree to our{" "}
+                <a href="#" className="font-semibold text-indigo-600">
+                  privacy&nbsp;policy
+                </a>
+                .
+              </Switch.Label>
+            </Switch.Group>
+          </div>
+          <div className="mt-6 pb-8">
+            <button
+              type="submit"
+              className=" bg-amber-600 block w-full rounded-md  px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={postData}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.1339499098685!2d76.6918822!3d30.714634499999992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fee543f626e9f%3A0x82baf1b646e9f236!2sd%2C%20185D%2C%20Phase%2C%208B%2C%20Phase%208B%2C%20Industrial%20Area%2C%20Sector%2074%2C%20Sahibzada%20Ajit%20Singh%20Nagar%2C%20Punjab%20140308!5e0!3m2!1sen!2sin!4v1697361285848!5m2!1sen!2sin"
+          allowfullscreen=""
+          loading="lazy"
+          className=" border-0 pt-2 pl-1 pb-5 pr-5 w-full h-full "
+          // referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
