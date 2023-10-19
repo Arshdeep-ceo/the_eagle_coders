e7d870ed67acb156279803607e0ba0a50e8d8dbd
Arshdeep-ceo <punjabistudios18@gmail.com>
2023-10-20 01:34:52 +0530

    //

--- src/features/home/use.jsx
+++ src/features/home/use.jsx
@@ -66,7 +66,7 @@ export default function Use() {
           to international coding standards and practices.
         </p>
 
-        <a className=" mt-4  inline-flex items-center px-3 py-2 text-sm font-medium  text-white bg-white/5 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-white/5 dark:hover:bg-white/10 dark:focus:ring-blue-800">
+        <p className=" mt-4  inline-flex items-center px-3 py-2 text-sm font-medium  text-white bg-white/5 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-white/5 dark:hover:bg-white/10 dark:focus:ring-blue-800">
           {" "}
           we{"'re"} not just teaching code.<br></br> we{"'re"} shaping the
           future architects of the web.
