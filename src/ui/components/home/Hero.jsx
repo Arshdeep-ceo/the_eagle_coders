9896b156109f2f1aa9e29c6714c9d5833c6598eb
Arshdeep-ceo <punjabistudios18@gmail.com>
2023-10-23 23:20:54 +0530

    ..

--- src/ui/components/home/Hero.jsx
+++ src/ui/components/home/Hero.jsx
@@ -52,7 +52,7 @@ function Hero({ setCurrentTab }) {
       <h2 className="font-semibold text-lg m-2 leading-3 text-center dark:text-gray-300 z-10">
         Master React, Ai, Flutter and more...
       </h2>
-      <p className=" text-gray-500 dark:text-gray-400 max-w-xl self-center p-3 text-center text-sm sm:p-6 z-10">
+      <p className=" text-gray-500 dark:text-gray-300 max-w-xl self-center p-3 text-center text-sm sm:p-6 z-10">
         We at Eagle Coders inspires to teach students the current market leading
         technologies<br></br>We Provide IT services and software products and AI
         Services. <br></br>Innovating today for a better digital tomorrow.
