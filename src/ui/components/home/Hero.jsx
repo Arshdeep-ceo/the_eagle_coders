9d1c0a49247a3670f2bbf2f26b5e20d1a2287b0f
Arshdeep-ceo <punjabistudios18@gmail.com>
2023-10-23 23:51:51 +0530

    //

--- src/ui/components/home/Hero.jsx
+++ src/ui/components/home/Hero.jsx
@@ -56,7 +56,7 @@ function Hero({ setCurrentTab }) {
         We at CodeRippleX inspires to teach students the current market leading
         technologies<br></br>We Provide IT services and software products and AI
         Services. <br></br>Innovating today for a better digital tomorrow.
-        <br></br>Unparalleled expertise meets unmatched dedication:<br></br>{" "}
+        <br></br>
         That{"'s"} the CodeRippleX promise to each of our valued clients.
       </p>
       <div className="flex items-center justify-center gap-x-3 flex-col gap-y-2 sm:flex-row z-10">
