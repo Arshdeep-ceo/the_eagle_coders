31978ba50ba341d5debaa87251f4eb480f7c1dab
Arshdeep-ceo <punjabistudios18@gmail.com>
2023-10-24 23:01:37 +0530

    ..

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
     // <section className="">
     // <div className="overflow-visible">
 
-    <div className="flex  flex-col p-0 m-0">
+    <div className="flex  flex-col p-0 m-0 overflow-x-clip">
       <a
         aria-label="Chat on WhatsApp"
         href="https://wa.me/916283669924?text=Hey%20there%20Eagle%20Coders%20"
