8f3589149d65301f9f37641de45ccaecbe60b067
Arshdeep-ceo <punjabistudios18@gmail.com>
2023-10-25 22:27:54 +0530

    ..

--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -35,13 +35,12 @@ export default function Contact() {
       <ContactDetails />
       <Contactform />
 
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d270749.72481984575!2d76.57867897943643!3d30.808395150261166!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sin!4v1697443684655!5m2!1sen!2sin"
+      {/* <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3430.2422507052315!2d76.6939939!3d30.7115894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fee561aa9133b%3A0x690bedf187a1dff!2sd%2C%20185D%2C%20Phase%208B%2C%20Industrial%20Area%2C%20Sector%2074%2C%20Sahibzada%20Ajit%20Singh%20Nagar%2C%20Punjab%20160055!5e0!3m2!1sen!2sin!4v1698169301783!5m2!1sen!2sin"
         className="  h-full rounded-lg"
         allowfullscreen=""
         loading="lazy"
-        // referrerpolicy="no-referrer-when-downgrade"
-      />
+      ></iframe> */}
     </div>
   );
 }
